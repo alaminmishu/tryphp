f6f0a4229d607a1c7da2d01d39f9700fdfc8aae3
Al Amin Mishu <aaamishucse@gmail.com>
2025-02-20 00:07:10 +0600

    Use method in the api route

--- testlaravel/routes/api.php
+++ testlaravel/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthenticatedSessionController::class . 'store']);
+Route::post('/logout', [AuthenticatedSessionController::class . 'destroy'])->middleware('auth:sanctum');
