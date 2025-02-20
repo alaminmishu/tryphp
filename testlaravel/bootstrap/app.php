a9c345fb62c1f7c60daa5be79dfbb314117c38ff
Al Amin Mishu <aaamishucse@gmail.com>
2025-02-20 11:47:47 +0600

    Error Handling

--- testlaravel/bootstrap/app.php
+++ testlaravel/bootstrap/app.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -27,5 +32,42 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function(ModelNotFoundException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Resource not found',
+            ], 404);
+        });
+        $exceptions->render(function(NotFoundHttpException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Resource not found',
+            ], 404);
+        });
+        $exceptions->render(function(AuthorizationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'You are not authorized to perform this action',
+            ], 403);
+        });
+        $exceptions->render(function(ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation failed',
+                'errors' => $e->errors(),
+            ], 422);
+        });
+        $exceptions->render(function(MethodNotAllowedException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Method is not allowed fot the requested route',
+            ], 405);
+        });
+        $exceptions->render(function(\Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'An error occurred',
+                'error' => $e->getMessage(),
+            ], 500);
+        });
     })->create();
